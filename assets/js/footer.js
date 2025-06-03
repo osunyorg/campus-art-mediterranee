f73e08138153a49a189f8dd40ea28dfce214891d
Olivia206 <olivia.simonet206@gmail.com>
2025-06-03 14:33:09 +0200

    wip: footer from home block

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,34 +1,34 @@
-class FooterShapes {
-    constructor () {
-        this.shapesContainer = document.querySelector('.footer-landscape');
-        if (!this.shapesContainer) {
-            return;
-        }
-        this.listen();
-    }
+// class FooterShapes {
+//     constructor () {
+//         this.shapesContainer = document.querySelector('.footer-landscape');
+//         if (!this.shapesContainer) {
+//             return;
+//         }
+//         this.listen();
+//     }
 
-    listen () {
-        this.images = [];
+//     listen () {
+//         this.images = [];
 
-        this.getImagesUrl();
-        this.createImage();
-    }
+//         this.getImagesUrl();
+//         this.createImage();
+//     }
 
-    getImagesUrl() {
-        for (let i = 0; i < 5; i++) {
-            let imageIndex = i + 1;
-            let imageUrl = `/assets/images/footer/image-${imageIndex}.jpg`;
-            this.images.push(imageUrl);
-        }
-    }
+//     getImagesUrl() {
+//         for (let i = 0; i < 5; i++) {
+//             let imageIndex = i + 1;
+//             let imageUrl = `/assets/images/footer/image-${imageIndex}.jpg`;
+//             this.images.push(imageUrl);
+//         }
+//     }
 
-    createImage () {
-        let image = document.createElement('img');
-        let url = this.images[Math.floor(Math.random() * this.images.length)];
+//     createImage () {
+//         let image = document.createElement('img');
+//         let url = this.images[Math.floor(Math.random() * this.images.length)];
 
-        image.src = url;
-        this.shapesContainer.appendChild(image);
-    }
-}
+//         image.src = url;
+//         this.shapesContainer.appendChild(image);
+//     }
+// }
 
-export default new FooterShapes();
+// export default new FooterShapes();
